a63e8ee7aa68d5052405a4febf2a5bb95d0708ec
Daisuke Nakamura <daisuke@example.com>
2011-08-07 20:21:16 +0900

    http://codeforces.com/contest/101/problem/A

---
+++ algo/0807/homework.rb
@@ -0,0 +1,29 @@
+#!ruby
+
+str = STDIN.gets.strip
+k = STDIN.gets.strip.to_i
+
+charnums = Hash.new(0)
+
+str.each_byte do |c|
+    charnums[c] += 1
+end
+
+delchar = []
+
+charnums.to_a.sort do |a,b|
+    (a[1] <=> b[1]) * 2 + (b[0] <=> a[0])    
+end.each do |elem|
+    if elem[1] < k
+        delchar << elem[0]
+        k -= elem[1]
+    else
+        break
+    end
+end
+
+delchar.each do |c|
+    str.delete("%c"%c)
+end
+
+p str
